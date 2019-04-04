402d42e92734f2b4fadb41fa4540ae19504650fa
Jean-Rene Choiniere <jeanrenechoiniere@gmail.com>
2019-04-04 14:39:11 -0400

    npc anims + navmesh + npccommand ability

---
+++ Assets/Scripts/AI/NPCCommand.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
+public class NPCCommand: MonoBehaviour
+{
+    private Image _image = null;
+
+    private NavMeshAgent _navMeshAgent = null;
+    private Animator _animator = null;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _image = GetComponentInChildren<Image>();
+        _animator = GetComponent<Animator>();
+        _image.fillAmount = 0.0f;
+    }
+
+    private void Update()
+    {
+        _animator.SetFloat("SpeedRatio", _navMeshAgent.velocity.magnitude / _navMeshAgent.speed);
+    }
+
+    public void MoveTo(Vector3 position)
+    {
+        _navMeshAgent.destination = position;
+    }
+
+    public void SetFillAmount(float value)
+    {
+        _image.fillAmount = value;
+    }
+
+    public void Command()
+    {
+        _animator.SetBool("WithLeg", true);
+        _navMeshAgent.destination = new Vector3(-15,-1,5);
+    }
+}
--- Assets/Scripts/Character/AbilityNpcCommander.cs
+++ Assets/Scripts/Character/AbilityNpcCommander.cs
@@ -3,11 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Character
 {
     public class AbilityNpcCommander: AbilityBase
     {
+        [SerializeField] private GameObject _targetAimObject = null;
+        [SerializeField] private GameObject _fireSocket = null;
+        [SerializeField] private float _fillAmountSpeed = 8.0f;
+
         public override Abilities Ability => Abilities.NPC_Commander;
+
+        private NPCCommand _currentNpcControlled = null;
+        private NPCCommand _currentCommandInProgress = null;
+        private float _currentFilledAmount = 0.0f;
+
+        private void Update()
+        {
+            RaycastHit hit;
+            var direction = (PlayerEyes.GetWorldLocation() - _fireSocket.transform.position).normalized;
+            if (Physics.Raycast(new Ray(_fireSocket.transform.position, direction), out hit))
+            {
+                _targetAimObject.transform.position = hit.point + hit.normal * 0.001f;
+                _targetAimObject.transform.rotation = Quaternion.LookRotation(hit.normal);
+
+                var command = hit.collider.GetComponentInParent<NPCCommand>();
+
+                if (_currentCommandInProgress && command != _currentCommandInProgress)
+                {
+                    _currentCommandInProgress.SetFillAmount(0.0f);
+                    _currentFilledAmount = 0.0f;
+                }
+
+                if (command)
+                {
+                    _currentFilledAmount = Mathf.MoveTowards(_currentFilledAmount, 1.0f, Time.deltaTime * _fillAmountSpeed);
+                    command.SetFillAmount(_currentFilledAmount);
+                    _currentCommandInProgress = command;
+                }
+
+                if (Math.Abs(_currentFilledAmount - 1.0f) < float.Epsilon)
+                {
+                    _currentNpcControlled = command;
+                    _currentFilledAmount = 0.0f;
+                    _currentNpcControlled.Command();
+                }
+            }
+        }
+        
     }
 }
