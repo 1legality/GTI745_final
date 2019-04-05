7b052578a415a58b9a059495a0d8ed0d43ce6f4a
Maxime Larivière <maxime.larivierem@gmail.com>
2019-04-05 00:54:43 -0400

    Gameloop

---
+++ Assets/Scripts/CoreGameplay/GameLoop.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameLoop : MonoBehaviour
+{
+    [SerializeField] private Text _GameText;
+    [SerializeField] private Text _TimerText;
+
+    [SerializeField] private float _TimerLimit;
+    [SerializeField] private float _CurrentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _CurrentTime = _TimerLimit;
+        _GameText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+
+        if(_CurrentTime > 0.0f)
+        {
+            _CurrentTime -= Time.deltaTime;
+        }
+        else
+        {
+            GameOver();
+        }
+
+        _TimerText.text = UnityEngine.Mathf.Round(_CurrentTime).ToString();
+    }
+    
+    public static void OnGameWin()
+    {
+        _GameText.text = "You win \n Your time is : " + (_TimerLimit - _CurrentTime) + " seconds";
+        _GameText.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void GameOver()
+    {
+        _GameText.text = "You lose";
+        _GameText.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
